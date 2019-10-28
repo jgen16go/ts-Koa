77e180d249bd245fa4263013c79442f8b9b151ce
jgen16go <asai.work.usr@gmail.com>
2019-10-28 13:57:35 +0900

    update

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@ export interface IConfig {
   port: number
 }
 
-// const isDev = process.env.NODE_ENV === 'development'
-
 const config: IConfig = {
   port: !process.env.PORT ? 3000 : parseInt(process.env.PORT, 10),
 }
